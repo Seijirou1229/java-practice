537abea30071a549994487232406826470a459ed
Seijirou Hashimoto <zama47seijirou@gmail.com>
2025-05-12 13:36:28 +0900

    Update VarianceArgs.java

--- VarianceArgs.java
+++ VarianceArgs.java
@@ -1,15 +1,15 @@
 class VarianceArgs{
 	public static void main(String[]args){
 		double sum = 0.0;
+	        double[] numbers = new double[args.length];
 		for(int i = 0; i < args.length; i++){
-		double num = Double.parseDouble(args[i]);
-		sum += num; 
+			numbers[i] = Double.parseDouble(args[i]);
+			sum += numbers[i]; 
 		} 
 		double average = sum / args.length;
-		double varianceSum = 0;
-		for(int k = 0; k < args.length; k++){
-		double num2 = Double.parseDouble(args[k]);
-		varianceSum += (num2 - average) * (num2 - average);
+		double varianceSum = 0.0;
+		for(int i = 0; i < args.length; i++){
+			varianceSum += (numbers[i] - average) * (numbers[i] - average);
 		}
 		double variance = varianceSum / args.length;
 		System.out.print("Variance:" + variance);
